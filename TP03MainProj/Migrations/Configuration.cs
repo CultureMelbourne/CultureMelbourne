c4ea9ebdf8d4046f1e22910eaa4d13eb6cf67e13
Minghao Tang <79492655+MinghaoTang1996@users.noreply.github.com>
2024-03-20 00:20:44 +1100

    Add project files.

---
+++ TP03MainProj/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace TP03MainProj.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TP03MainProj.Models.MyApplicationContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TP03MainProj.Models.MyApplicationContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ TP03MainProj/Models/MyApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TP03MainProj.Models
+{
+    public class MyApplicationContext : DbContext
+    {
+        public MyApplicationContext() : base("name=DefaultConnection") { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
