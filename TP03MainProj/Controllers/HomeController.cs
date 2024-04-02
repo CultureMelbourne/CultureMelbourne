1863a28596df7c4980997be9152a60d99bf65fb3
Minghao Tang <79492655+MinghaoTang1996@users.noreply.github.com>
2024-04-03 00:33:21 +1100

    132

--- TP03MainProj/Controllers/HomeController.cs
+++ TP03MainProj/Controllers/HomeController.cs
@@ -12,10 +12,7 @@ namespace TP03MainProj.Controllers
         {
             return View();
         }
-        public ActionResult Home()
-        {
-            return View();
-        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
