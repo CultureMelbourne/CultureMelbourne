9eb1b8f202bba86f1f36501f78d3f1ebf2afd0c1
Minghao Tang <79492655+MinghaoTang1996@users.noreply.github.com>
2024-04-06 22:53:54 +1100

    epic 3

---
+++ TP03MainProj/Controllers/EducationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TP03MainProj.Controllers
+{
+    public class EducationController : Controller
+    {
+        // GET: Education
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DiverseCultures()
+        {
+            return View();
+        }
+
+        public ActionResult MigrationStories()
+        { 
+            return View();
+
+        }
+}
+}
