03d349418bc7a85c45ef73f832034d6c6adc72ea
Minghao Tang <79492655+MinghaoTang1996@users.noreply.github.com>
2024-04-20 16:19:18 +1000

    remove about and contact

--- TP03MainProj/Controllers/HomeController.cs
+++ TP03MainProj/Controllers/HomeController.cs
@@ -39,18 +39,5 @@ namespace TP03MainProj.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
