19cd3070f804d5aa40fd2b821098d8b41d1a8f0d
Minghao Tang <79492655+MinghaoTang1996@users.noreply.github.com>
2024-05-17 01:18:11 +1000

    css

--- TP03MainProj/Scripts/scrollToTop.js
+++ TP03MainProj/Scripts/scrollToTop.js
@@ -10,6 +10,22 @@
 
     // Animate the scroll to the top when the button is clicked
     $('#scrollToTopBtn').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 'slow');
+        $('html, header').animate({ scrollTop: 0 }, 'slow');
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var toggler = document.querySelector(".header_navbar-toggler");
+    var sidebar = document.getElementById("header_sidebar");
+    var closeSidebar = document.querySelector(".close-sidebar");
+
+    toggler.addEventListener("click", function () {
+        sidebar.classList.toggle("show");
+        toggler.classList.toggle("hidden");
+    });
+
+    closeSidebar.addEventListener("click", function () {
+        sidebar.classList.toggle("show");
+        toggler.classList.toggle("hidden");
     });
 });
