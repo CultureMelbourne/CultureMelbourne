771a468c58437fae090e0cf9d4c1782e68cf02db
Minghao Tang <79492655+MinghaoTang1996@users.noreply.github.com>
2024-05-16 14:14:41 +1000

    file

--- TP03MainProj/Controllers/EventsController.cs
+++ TP03MainProj/Controllers/EventsController.cs
@@ -80,45 +80,6 @@ namespace TP03MainProj.Controllers
         }
 
 
-
-
-
-
-        // To retrieve data from API (using static DB for the time being)
-        //public JsonResult GetEventsFromEventbrite(string culture, DateTime date)
-        //{
-        //    string filePath = Server.MapPath("~/Content/DataSource/melbourne_events.csv");
-
-        //    var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-        //    {
-        //        HasHeaderRecord = true,
-        //        Delimiter = ",",
-        //        BadDataFound = null,
-        //    };
-
-        //    using (var reader = new StreamReader(filePath))
-        //    using (var csv = new CsvReader(reader, config))
-        //    {
-        //        csv.Context.RegisterClassMap<EventMap>();
-        //        var records = csv.GetRecords<Events>().ToList();
-
-        //        var filteredEvents = records
-        //            .Where(e => e.Culture.Equals(culture, StringComparison.OrdinalIgnoreCase) &&
-        //                        date >= e.StartDate &&
-        //                        date <= e.EndDate)
-        //            .Select(e => new
-        //            {
-        //                title = e.Title,
-        //                start = e.StartDate.ToString("yyyy-MM-dd"),
-        //                end = e.EndDate.ToString("yyyy-MM-dd"),
-        //                description = e.Description,
-        //                url = e.Url
-        //            }).ToList();
-
-        //        return Json(filteredEvents, JsonRequestBehavior.AllowGet);
-        //    }
-        //}
-
         public ActionResult GetEventsFromEventbrite(string culture, DateTime date)
         {
             string filePath = Server.MapPath("~/Content/DataSource/melbourne_events.csv");
---
+++ TP03MainProj/Helper/ImageHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace TP03MainProj.Helper
+{
+    public class ImageHelper
+    {
+        public static string GetImagePath(string title)
+        {
+            var imageExtensions = new List<string> { ".png", ".jpg", ".jpeg", ".webp" };
+            var basePath = HttpContext.Current.Server.MapPath("~/Content/Images/events/");
+            //title = title.Replace(" ", "_");
+
+            foreach (var ext in imageExtensions)
+            {
+                var filePath = Path.Combine(basePath, title + ext);
+                Debug.WriteLine("Checking file: " + filePath);
+
+                if (File.Exists(filePath))
+                {
+                    Debug.WriteLine("File found: " + filePath);
+                    return "~/Content/Images/events/" + title + ext;
+                }
+            }
+            Debug.WriteLine("No matching file found for title: " + title);
+            return "~/Content/Customer/images/services/1.webp"; // Path to a default image if no matching file is found
+        }
+    }
+}
